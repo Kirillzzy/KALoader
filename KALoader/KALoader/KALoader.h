3e20f75878890dbc7a111b080c866b132302869c
Kirill <kirillzzy@gmail.com>
2017-09-08 21:05:16 +0300

    Added Xcode project

---
+++ KALoader/KALoader/KALoader.h
@@ -0,0 +1,19 @@
+//
+//  KALoader.h
+//  KALoader
+//
+//  Created by Kirill Averyanov on 08/09/2017.
+//  Copyright © 2017 Kirill Averyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KALoader.
+FOUNDATION_EXPORT double KALoaderVersionNumber;
+
+//! Project version string for KALoader.
+FOUNDATION_EXPORT const unsigned char KALoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KALoader/PublicHeader.h>
+
+
